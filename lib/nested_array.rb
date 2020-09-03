732279fcdc9253e8fd3ab320a93090ad5793843c
Erick Villeda <eeverickeev@gmail.com>
2020-09-03 17:30:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,12 +31,8 @@ def matrix_lookup(matrix, row, column)
   empty_array = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
   matrix[column][row]
 end
-
-# Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
   
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+  empty_array = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  matrix[column][row] = new_value
 end
